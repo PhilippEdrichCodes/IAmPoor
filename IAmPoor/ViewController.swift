e24b32690b0bd5827c72173580973539aa38a100
PhilippEdrichCodes <philipp@edrich.codes>
2022-11-23 16:58:37 +0100

    Initial Commit

---
+++ IAmPoor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IAmPoor
+//
+//  Created by Philipp Edrich on 23.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
